c8a89835803010fcd5927f3ace90ce539b602583
Steven McWilliams <skm@Stevens-MacBook-Air.local>
2020-01-06 21:25:16 -0600

    Fourth commit

--- GDP.py
+++ GDP.py
@@ -1,19 +1,42 @@
 import pandas as pd
+import numpy as np
 from bokeh.plotting import figure, output_file, save
 
-gdp = pd.read_csv('GDP.csv')
+gdp_data = pd.read_csv('GDP.csv')
 unemployed = pd.read_csv('Unemployment.csv')
-x=gdp[['date']]
-gdp_change=gdp[['change-current']]
+x=gdp_data[['date']]
+gdp=gdp_data[['change-current']]
 unemployment=unemployed[['unemployment']]
 title='Correlation Between US GDP and Unemployment'
-file_name="GDP.html"
+file_name= 'GDPvsUnemployment.html'
 
+#MAKE DATA ONE DIMENTIONAL FOR STATISTICAL PURPOSES
+gdp_condensed = np.squeeze(np.array(gdp))
+unemployment_condensed = np.squeeze(np.array(unemployment))
+par = np.polyfit(gdp_condensed, unemployment_condensed,1, full=True)
+slope=par[0][0]
+intercept=par[0][1]
+y_predicted = [slope*i + intercept  for i in gdp_condensed]
+
+#CREATE GDP AND UNEMPLOYMENT LINE GRAPHS
 p = figure(title=title, x_axis_label='year', y_axis_label='%')
-p.line(x.squeeze(), gdp_change.squeeze(), color="firebrick", line_width=4, legend="% GDP change")
-p.line(x.squeeze(), unemployment.squeeze(), line_width=4, legend="% unemployed")
+p.line(x.squeeze(), gdp.squeeze(), color="red", line_width=2, legend=" GDP ")
+p.line(x.squeeze(), unemployment.squeeze(), line_width=2, legend="% unemployed")
 output_file(file_name)
-save(p)
+
+#TRYING TO ADD MORE DATA TO GRAPH - BELOW DATA PRINTS TO TERMINAL
+covariance = np.cov(gdp_condensed, unemployment_condensed, bias=True)[0][1]
+correlation = np.corrcoef(gdp_condensed, unemployment_condensed)[0, 1]
+cov = ('Covariance: ' + str(covariance))
+cc = ('Correlation: ' + str(correlation))
+print(cc)
+print(cov)
+
+try:
+    save(p)
+    print('Chart saved, open ' + str(file_name )+ ' to view')
+except: print('Chart could not print, please retry')
+
     
 
 
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-The GDP Python program will pull GDP YoY change from one CSV file and Unemployment Yoy change from the other CSV file. This will then be
-plotted using bokeh and output into an html file tilte "GDP.html"
+The GDP Python program will pull the annual GDP from one CSV file and Unemployment from the other CSV file. This will then be plotted using bokeh and output into an html file tilted "GDPvsUnemployment.html"
 
 The second Python file reads the Unemployment CSV and plots the five highest years of unemploymenet along with the rows that data occupies and the percent of unemployment for that year
